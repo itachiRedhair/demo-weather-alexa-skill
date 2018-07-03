1c676cdbe79d0452adab8e57a8e10ef1c045bdb7
itachiRedhair <akshay.milmile@gmail.com>
2018-07-03 13:24:57 +0530

    updated

--- lambda/custom/handlers/Launch.js
+++ lambda/custom/handlers/Launch.js
@@ -5,7 +5,7 @@ const LaunchRequestHandler = {
 
   handle(handlerInput) {
     const prompt = `Welcome to your weather skill.`;
-    const reprompt = `Here's the weather in London.`;
+    const reprompt = `Ask for weather in London.`;
 
     return handlerInput.responseBuilder
       .speak(prompt + ` ` + reprompt)
