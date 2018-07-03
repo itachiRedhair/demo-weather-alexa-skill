0616f6ceac607a6880dee4e5bd40d02e19ad359e
itachiRedhair <akshay.milmile@gmail.com>
2018-07-03 13:26:52 +0530

    updated

--- lambda/custom/handlers/General.js
+++ lambda/custom/handlers/General.js
@@ -1,16 +1,12 @@
 const SessionEndedRequestHandler = {
   canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === "SessionEndedRequest";
+    return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
   },
   handle(handlerInput) {
-    console.log(
-      `Session ended with reason: ${
-        handlerInput.requestEnvelope.request.reason
-      }`
-    );
+    console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
 
     return handlerInput.responseBuilder.getResponse();
-  }
+  },
 };
 
 const ErrorHandler = {
@@ -24,7 +20,7 @@ const ErrorHandler = {
       .speak("Sorry, I can't understand the command. Please say again.")
       .reprompt("Sorry, I can't understand the command. Please say again.")
       .getResponse();
-  }
+  },
 };
 
 module.exports = { SessionEndedRequestHandler, ErrorHandler };
--- lambda/custom/handlers/Launch.js
+++ lambda/custom/handlers/Launch.js
@@ -1,6 +1,6 @@
 const LaunchRequestHandler = {
   canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === "LaunchRequest";
+    return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
   },
 
   handle(handlerInput) {
@@ -11,7 +11,7 @@ const LaunchRequestHandler = {
       .speak(prompt + ` ` + reprompt)
       .reprompt(reprompt)
       .getResponse();
-  }
+  },
 };
 
 module.exports = { LaunchRequestHandler };
