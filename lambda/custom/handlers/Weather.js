7107bf8dbb2aab1d5e9a8572e737a99a56f3c9ff
itachiRedhair <akshay.milmile@gmail.com>
2018-07-05 08:42:49 +0530

    'updated'

--- lambda/custom/handlers/Weather.js
+++ lambda/custom/handlers/Weather.js
@@ -2,12 +2,18 @@ const TellWeatherHandler = {
   canHandle(handlerInput) {
     const { request } = handlerInput.requestEnvelope;
 
-    return request.type === intents.type.IntentRequest && request.intent.name === 'TellWeather';
+    return request.type === 'IntentRequest' && request.intent.name === 'TellWeather';
   },
 
   async handle(handlerInput) {
-    // TODO: extract slots from request and pass it to fetchWeather api and get response and tell the response
-    //
+    const citySlot = handlerInput.requestEnvelope.request.intent.slots.city;
+    let cityName;
+    if (citySlot && citySlot.value) {
+      cityName = citySlot.value.toLowerCase();
+    }
+
+    console.log(cityName);
+
     return handlerInput.responseBuilder
       .speak(`Here's the weather in London.`)
       .reprompt(`Ask me about weather in New York.`)
