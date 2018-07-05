68fd38e50bd23d138e596af48add6a16bfdb5b65
itachiRedhair <akshay.milmile@gmail.com>
2018-07-05 09:00:29 +0530

    'updated'

--- lambda/custom/handlers/Weather.js
+++ lambda/custom/handlers/Weather.js
@@ -1,3 +1,5 @@
+const fetchWeather = require('./../utilities/api/fetchWeather');
+
 const TellWeatherHandler = {
   canHandle(handlerInput) {
     const { request } = handlerInput.requestEnvelope;
@@ -7,15 +9,29 @@ const TellWeatherHandler = {
 
   async handle(handlerInput) {
     const citySlot = handlerInput.requestEnvelope.request.intent.slots.city;
+
     let cityName;
     if (citySlot && citySlot.value) {
       cityName = citySlot.value.toLowerCase();
     }
 
-    console.log(cityName);
+    let weatherInfo;
+    if (cityName) {
+      weatherInfo = await fetchWeather(cityName);
+    } else {
+      // TODO: Get Weather based on device address
+    }
+
+    let prompt;
+    if (weatherInfo) {
+      const temperature = weatherInfo.main.temp - 273;
+      prompt = `Currently the temperature in london in ${temperature.toFixed(2)} celsius.`;
+    } else {
+      prompt = 'Sorry no weather information is available for this city.';
+    }
 
     return handlerInput.responseBuilder
-      .speak(`Here's the weather in London.`)
+      .speak(prompt)
       .reprompt(`Ask me about weather in New York.`)
       .getResponse();
   }
--- lambda/custom/utilities/api/fetchWeather.js
+++ lambda/custom/utilities/api/fetchWeather.js
@@ -1,1 +1,15 @@
-// TODO: Write an API to fetch weather data with input of city
+const fetch = require('node-fetch');
+
+const API_KEY = '4bab2df8f5a678cbe86ac01e5203de87';
+
+const fetchWeather = async cityName => {
+  const response = await fetch(
+    `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`
+  );
+
+  const weatherInfo = await response.json();
+
+  return weatherInfo;
+};
+
+module.exports = fetchWeather;
