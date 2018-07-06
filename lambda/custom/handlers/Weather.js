2e18414e275f85628aa0c62bd9fbc5565a78466c
itachiRedhair <akshay.milmile@gmail.com>
2018-07-06 12:00:43 +0530

    updated

--- lambda/custom/handlers/Weather.js
+++ lambda/custom/handlers/Weather.js
@@ -31,7 +31,7 @@ const TellWeatherHandler = {
     let prompt;
     if (weatherInfo) {
       const temperature = weatherInfo.main.temp - 273;
-      prompt = `Currently the temperature in london in ${temperature.toFixed(2)} celsius.`;
+      prompt = `Currently the temperature in ${city} in ${temperature.toFixed(2)} celsius.`;
     } else {
       prompt = 'Sorry no weather information is available for this city.';
     }
@@ -40,7 +40,7 @@ const TellWeatherHandler = {
       .speak(prompt)
       .reprompt(`Ask me about weather in New York.`)
       .getResponse();
-  }
+  },
 };
 
 module.exports = { TellWeatherHandler };
